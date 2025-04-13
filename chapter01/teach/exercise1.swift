ffc12c1a74b3654b3b888af4dff201b1e7156883
shawn seo <shawnseo@shawns-MacBook-Pro.local>
2025-04-13 11:27:48 -0700

    Added exercise1.swift with Swift code

---
+++ chapter01/teach/exercise1.swift
@@ -0,0 +1,33 @@
+let myAge: Int = 17
+var averageAge: Double = 17
+/* im now finding the average of my age and a 30 yrold*/
+averageAge = Double ( myAge + 30 ) / 2
+/* now moving onto step 3*/
+let testNumber: Int = 12
+let evenOdd: Int = testNumber % 2
+var answer: Int = 0
+answer += 1
+answer = answer + 10
+answer = answer >> 3
+
+print("these are the chapter 1 problems")
+
+
+/* challenge #1 */
+let myAge: Int = 17
+var dogs: Int = 1
+dogs += 1
+
+/*challenge #2*/
+var age: Int = 16
+print ( age )
+age = 30
+print ( age )
+
+/*challenge #3*/
+let x: Int = 46
+let y: Int = 10
+let answer1: Int = ( x * 100 ) + y
+//easy challenge beat//
+
+
