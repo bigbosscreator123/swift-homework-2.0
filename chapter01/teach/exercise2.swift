d952b104a1eb7fa88f360d6c2026d54d4c854935
shawn seo <shawnseo@shawns-MacBook-Pro.local>
2025-04-13 11:33:45 -0700

    Added exercise2.swift for Chapter 2

---
+++ chapter01/teach/exercise2.swift
@@ -0,0 +1,42 @@
+let age1 = Double ( 42 )
+let age2 = Double ( 21 )
+
+let avg1 = ( age1 + age2 ) / 2
+
+var message = "hello " + "my name is "
+let name = "Shawn"
+message += name
+let exclamationMark: Character = "!"
+message += String ( exclamationMark )
+
+let oneTwo = 1.0 / 2.0
+let oneTwoEnglish = "one over two is \(oneTwo) as a decimal"
+
+let nameList = """
+Shawn 
+Mikala
+John
+Sunmi
+Liz
+Ethan
+Fred
+Jenna
+"""
+
+let firstName: String = "Shawn"
+let lastName: String = "Seo"
+let fullName: String = "\(firstName) \(lastName)"
+let myDetails = "Hello, my name is \(fullName) and I aspire to be a CS master"
+print (myDetails)
+
+
+
+let coordinates = (2,3)
+let namedCoordinate = (row: 2, column: 3)
+
+let tuple = (day: 15, month: 8, year: 2015)
+let day = tuple.day
+
+var name = "Shawn"
+name += " Seo"
+
